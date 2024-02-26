10fd9d8028be3a0a7b445500082f18bc33e7908c
Alexandre <89224363+helloAle@users.noreply.github.com>
2024-02-26 00:24:12 -0300

    Update script.js

--- script.js
+++ script.js
@@ -26,21 +26,21 @@ buttonClose.onclick = function () {
 }
 
 // Função para iniciar a contagem regressiva
-function startCountdown() {
-    var countdownElement = document.getElementById('countdown');
-    var contentElement = document.getElementById('content');
-    var count = 9;
+// function startCountdown() {
+//     var countdownElement = document.getElementById('countdown');
+//     var contentElement = document.getElementById('content');
+//     var count = 9;
     
-    var countdownInterval = setInterval(function() {
-      countdownElement.textContent = count;
-      count--;
+//     var countdownInterval = setInterval(function() {
+//       countdownElement.textContent = count;
+//       count--;
 
-      if (count < 0) {
-        clearInterval(countdownInterval);
-        countdownElement.style.display = 'none';
-        contentElement.style.display = 'block';
-      }
-    }, 1000);
-  }
+//       if (count < 0) {
+//         clearInterval(countdownInterval);
+//         countdownElement.style.display = 'none';
+//         contentElement.style.display = 'block';
+//       }
+//     }, 1000);
+//   }
 
-  window.onload = startCountdown;
+//   window.onload = startCountdown;
